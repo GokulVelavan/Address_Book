88af4795ab0c4b5af7b3d411e9f120b17292b1d6
GokulVelavan <81863053+GokulVelavan@users.noreply.github.com>
2021-11-27 20:39:30 +0530

    add data in JSON file

---
+++ AddressBookJSON.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Globalization;
+using CsvHelper;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Address_Book
+{
+    class AddressBookJSON
+    {
+       
+        public void ReadingandWritingJson()
+        {
+            
+            string path = @"C:\Users\INFINITY\source\repos\Address_Book\AddressBookFileCSV.csv";
+
+
+            var reader = new StreamReader(path);
+            var csvRead = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var output = csvRead.GetRecords<ContactList>().ToList();
+            foreach (ContactList details in output)
+            {
+                Console.WriteLine("FirstName: {0}\tLastName: {1}\tAddress: {2}\tCity: {3}\tState: {4}\tZipCode: {5}\tPhoneNumber: {6}\tEmail: {7}", details.firstName, details.lastName, details.address, details.city, details.state, details.zipCode, details.phoneNumber, details.email);
+            }
+
+            string JSONpath = @"C:\Users\INFINITY\source\repos\Address_Book\AddressBookFileJSON.json";
+
+            JsonSerializer serializer = new JsonSerializer();
+            using (StreamWriter sw = new StreamWriter(JSONpath))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, output);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -15,11 +15,12 @@ namespace Address_Book
             Console.WriteLine("Welcome to Address Book.");
             AddressBook obj1 = new AddressBook(); //creating object
             AddressBookCSV obj2 = new AddressBookCSV();
+            AddressBookJSON obj3 = new AddressBookJSON();
             Dictionary<string, AddressBook> addressBook_Dictionary = new Dictionary<string,AddressBook>();
             while (decide)
             {
 
-                Console.WriteLine("1.AddNewContact\n2.PrintContact\n3.EditContact\n4.DeleteContact\n5.Search by City\n6.Search by state\n7.getcount by city or state\n8.Sort by Name\n9.Sort by data\n10.Write Data in File \n11.Read Data in File \n12.write data in CSV file \n13.read datain csv file\n14.Exit");
+                Console.WriteLine("1.AddNewContact\n2.PrintContact\n3.EditContact\n4.DeleteContact\n5.Search by City\n6.Search by state\n7.getcount by city or state\n8.Sort by Name\n9.Sort by data\n10.Write Data in File \n11.Read Data in File \n12.write data in CSV file \n13.read datain csv file\n14.read and write json file\n15.Exit");
                 Console.WriteLine("\nPlease Enter your choice");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
@@ -75,6 +76,9 @@ namespace Address_Book
                         obj2.ReadeDataInCSVFile();
                         break;
                     case 14:
+                        obj3.ReadingandWritingJson();
+                        break;
+                    case 15:
                         decide = false;//code to exit the loop
                         break;
                     default:
